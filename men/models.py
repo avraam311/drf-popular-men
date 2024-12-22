2bc67142c39713abe114902d36065be93919a624
avraam311 <yandaroviba@mail.ru>
2024-12-22 22:26:40 +0300

    bug fix 1

--- men/models.py
+++ men/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Men(models.Model):
+# class Men(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
